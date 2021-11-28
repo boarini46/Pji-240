af2fcbbaa18aedfd9a5c84985637b078a3fc676e
pji110-2021grupo5 <pji110.2021grupo5@gmail.com>
2021-11-28 08:49:14 -0300

    ajuste no Flask __name__

--- new/flask_teste.py
+++ new/flask_teste.py
@@ -12,7 +12,7 @@ from pathlib import Path
 app = Flask(__name__)
 
 if __name__=='__main__':
-    app.run(debug=True)
+    app.run(debug=False)
 
 #port = int(os.getenv('PORT'),'5000')
 #app.run(host='0.0.0.0',port=port)
