59de083c80c5a3ca14d31e7e33d3f4198765303b
pji110-2021grupo5 <pji110.2021grupo5@gmail.com>
2021-11-28 08:38:59 -0300

    ajuste main app

--- new/flask_teste.py
+++ new/flask_teste.py
@@ -11,8 +11,8 @@ from pathlib import Path
 
 app = Flask(__name__)
 
-#if __name__=='__main__':
-#    app.run(debug=True)
+if __name__=='__main__':
+    app.run(debug=True)
 
 #port = int(os.getenv('PORT'),'5000')
 #app.run(host='0.0.0.0',port=port)
